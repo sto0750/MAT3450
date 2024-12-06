ddd50485d707a4c7607822a10fccb5663b107a93
sto0750 <138195562+sto0750@users.noreply.github.com>
2024-12-06 17:33:10 +0900

    Update ETS.py

--- ETS.py
+++ ETS.py
@@ -57,19 +57,19 @@ class ETS :
 
 
 # Load data
-districts = ['강남구', '강동구', '강북구', '강서구', '관악구', '광진구', '구로구', '금천구', '노원구',
-             '도봉구', '동대문구', '동작구', '마포구', '서대문구', '서초구', '성동구', '성북구', '송파구',
-             '양천구', '영등포구', '용산구', '은평구', '종로구', '중구', '중랑구']
+# districts = ['강남구', '강동구', '강북구', '강서구', '관악구', '광진구', '구로구', '금천구', '노원구',
+#              '도봉구', '동대문구', '동작구', '마포구', '서대문구', '서초구', '성동구', '성북구', '송파구',
+#              '양천구', '영등포구', '용산구', '은평구', '종로구', '중구', '중랑구']
 
-district_of_interest = districts[1]
+# district_of_interest = districts[1]
 
-df = pd.read_csv('seoul_data_csv/{}_data_filled.csv'.format(district_of_interest))
-print(district_of_interest)
+# df = pd.read_csv('seoul_data_csv/{}_data_filled.csv'.format(district_of_interest))
+# print(district_of_interest)
 
-ETS = ETS(df)
-ETS.preset()
-ETS.train()
-ETS.predict()
-ETS.eval()
-ETS.plot()
+# ETS = ETS(df)
+# ETS.preset()
+# ETS.train()
+# ETS.predict()
+# ETS.eval()
+# ETS.plot()
 
